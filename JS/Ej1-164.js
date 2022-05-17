5496545a2175d0e85ce0543ca1368c04f8bdf977
JustBM <105198476+JustBM@users.noreply.github.com>
2022-05-17 13:57:00 +0200

    Ejercicio 1 17/05

---
+++ JS/Ej1-164.js
@@ -0,0 +1,8 @@
+const d = (1);
+const e = (2);
+const f = (3);
+
+let a = 1, b = 3, c = 6; 
+console.log(a+b+c);
+c=17;
+console.log(a+b+c);
---
+++ JS/Var-JS.js
@@ -0,0 +1,13 @@
+// kebap-case (no permitido en JavaScript)
+// const last-name = 'Doe';
+// snake_case (recomendado en otros lenguajes como Python)
+const last_name = 'Last_Name';
+// PascalCase (recomendado en JavaScript para las clases)
+const LastName = 'LastName';
+// camelCase (recomendado en JavaScript para las variables y funciones)
+const lastName = 'lastname';
+document.write(LastName);
+let x; //creando variable;
+let y;
+x = 5; //ASignando valor de tipo numerico entero y valor 5;
+y = "texto"; //ASignando valor de tipo string y valor Texto;
