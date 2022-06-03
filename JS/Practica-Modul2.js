6716ae99754ecd0dbc22a8a03fc55d9331016b13
JustBM <bueno.jm92@gmail.com>
2022-06-03 13:56:12 +0200

    Examen

---
+++ JS/Practica-Modul2.js
@@ -0,0 +1,21 @@
+// gestor,
+// cliente, mensaje y transferencia.
+
+const gest1 = {
+    gestor:'BBVA',
+    cliente:'María',
+    Mensaje:'Para comprar el pan',
+    Transferencia:'500€'
+};
+const gest2 = {
+    gestor:'La Caixa',
+    cliente:'Hernesto',
+    Mensaje:'Hipoteca',
+    Transferencia:'1000€'
+};
+const gest3 = {
+    gestor:'ING',
+    cliente:'Rosa',
+    Mensaje:'Gym enero',
+    Transferencia:'40€'
+};
